abb04b3ae9e038ffead8717ce1164a810ba22054
murilodpl <mdpleopoldino@gmail.com>
2022-09-29 19:55:14 -0300

    feat: create Commands handler

---
+++ commands/joke.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('joke')
+        .setDescription('Replies with dirty joke!'),
+    async execute(interaction) {
+        let joke = await requestJoke();
+        return interaction.reply(joke);
+    },
+};
+
+async function requestJoke() {
+    let response = await request('https://v2.jokeapi.dev/joke/Any', 'get');
+    let data = response.data;
+    let joke = '';
+
+    if (data.type == 'twopart') {
+        let setup = data.setup;
+        let delivery = data.delivery;
+
+        joke = setup + '\n' + delivery
+    } else {
+        joke = data.joke;
+    }
+
+    return joke;
+}
+
+function request(url, method, data, cookie = '') {
+    return axios({ url, method, data, headers: { Authorization: `Bearer ${cookie}` }, validateStatus: false, withCredentials: true });
+};
